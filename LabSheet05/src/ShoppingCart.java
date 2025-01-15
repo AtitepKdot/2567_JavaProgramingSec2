fae5833dabe633564130ea2e6440cce38b1aa32a
AtitepKdot <fogus7748@gmail.com>
2025-01-15 10:30:09 +0700

    LabSheet05

---
+++ LabSheet05/src/ShoppingCart.java
@@ -0,0 +1,15 @@
+
+public class ShoppingCart {
+
+	public static void main(String[] args) {
+		// call Method displayCartItemAndTotal
+		displayCardAndTotal("Apple",10.5,"Banana",5.75,"Orange",7.3);
+		
+		
+		
+	}//end of mainMethod
+	public static void displayCardAndTotal(Object... item) {
+		
+	}//end of displayCardAndTotalMethod
+
+}
---
+++ LabSheet05/src/lab503.java
@@ -0,0 +1,9 @@
+
+public class lab503 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
