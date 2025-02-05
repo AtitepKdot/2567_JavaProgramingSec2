4079d2d5344de8a8e9ca1534e2433f89b8bd4be2
AtitepKdot <fogus7748@gmail.com>
2025-02-05 11:25:02 +0700

    Lab09Practice

---
+++ LabSheet09/src/Movie.java
@@ -0,0 +1,12 @@
+
+public class Movie {
+private String id;
+private String name;
+private Director director;
+Movie(String id, String name, Director director) {
+	this.id = id;
+	this.name = name;
+	this.director =director;
+}
+
+}
